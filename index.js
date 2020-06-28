5dd3181c6a979d0ee6994d8db59f45b5431e874b
Hammad-23 <62185607+Hammad-23@users.noreply.github.com>
2020-06-28 16:29:43 +0500

    Finance-app

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 
-
+console.log(firebase)
 function signUp() {
     const name = document.getElementById("name").value
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
-    let user = {
-        name: name,
-        email: email,
-        password: password
-    }
-  localStorage.setItem("user",JSON.stringify(user))
-  const data = localStorage.getItem("user")
-  const user2 = JSON.parse(data)
-  window.location.href = "index.html"
+
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(){
+      alert("Successfully Registered!")
+      window.location.href = "index.html"
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      alert(errorMessage)
+    });
+
+
+  //   let user = {
+  //       name: name,
+  //       email: email,
+  //       password: password
+  //   }
+  // // localStorage.setItem("user",JSON.stringify(user))
+  // const data = localStorage.getItem("user")
+  // const user2 = JSON.parse(data)
+  
 
 
 }
@@ -22,12 +38,26 @@ function logIn() {
   
 const userEmail = document.getElementById("userEmail").value
 const userPass = document.getElementById("userPass").value
-const data = localStorage.getItem("user")
-  const user2 = JSON.parse(data)
-if(userEmail===user2.email && userPass===user2.password){
-    alert("log in successfully")
-    window.location.href = "src/transaction/transaction.html"
-}
+
+firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+.then(function(user){
+  alert("Successfully Logged In")
+  window.location.href = "src/transaction/transaction.html"
+})
+.catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+  alert(errorMessage)
+});
+
+// const data = localStorage.getItem("user")
+//   const user2 = JSON.parse(data)
+// if(userEmail===user2.email && userPass===user2.password){
+//     alert("log in successfully")
+//    
+// }
 
 
 }
--- src/transaction/transaction.js
+++ src/transaction/transaction.js
@@ -53,28 +53,74 @@ const mySelect = document.getElementById("mySelect").value
 // column1.innerHTML = inputDate
 // column3.innerHTML = inputAmount
 // column2.innerHTML = mySelect
-
+const tableDiv = document.getElementById("tableDiv")
 
 
 const table = document.getElementById("table")
+  tableDiv.appendChild(table)
+table.setAttribute("class","table")
+const thead = document.createElement("THEAD")
+ table.appendChild(thead)
 
 
 const tr =  document.createElement("TR")
-const th =  document.createElement("TH")
-// let tableDiv = document.getElementById("tableDiv")
-
-// let di =  table.appendChild(table)
-// Console.log(di)
-const trVal =  table.appendChild(tr)
-
-trVal.innerHTML = inputAmount
+tr.setAttribute("id","t-r")
+  thead.appendChild(tr)
+const th1 =  document.createElement("TH")
 
 
 
-// Console.log(trVal)
-const thVal =  trVal.appendChild(th)
-// console.log(thVal)
-thVal.innerHTML = inputDate
+
+
+
+th1.innerHTML = "#"
+th1.setAttribute("scope","col")
+const th2 = document.createElement("TH")
+th2.innerHTML = "Date"
+th2.setAttribute("scope","col")
+const th3 = document.createElement("TH")
+th3.innerHTML = "Amount"
+th3.setAttribute("scope","col")
+const th4 = document.createElement("TH")
+th4.innerHTML = "Category"
+th4.setAttribute("scope","col")
+
+
+tr.appendChild(th1)
+tr.appendChild(th2)
+tr.appendChild(th3)
+tr.appendChild(th4)
+
+
+let tableBody = document.createElement("TBODY")
+
+let tableRow1 = document.createElement("TR")
+  tableBody.appendChild(tableRow1)
+let th11 = document.createElement("TH")
+  tableRow1.appendChild(th11)
+  th11.setAttribute("scope","row")
+
+
+const td1 = document.createElement("TD")
+th11.appendChild(td1)
+td1.innerHTML = inputDate
+const td2 = document.createElement("TD")
+td2.innerHTML = inputAmount
+th11.appendChild(td2)
+const td3 = document.createElement("TD")
+td3.innerHTML = mySelect
+th11.appendChild(td3)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
