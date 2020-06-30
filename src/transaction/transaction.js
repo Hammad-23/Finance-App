3c94589ad4b8667eb21f42a1b14e4af7b8ecfc5e
Hammad-23 <62185607+Hammad-23@users.noreply.github.com>
2020-06-30 20:43:25 +0500

    Finance App by javaScript

--- src/transaction/transaction.js
+++ src/transaction/transaction.js
@@ -53,65 +53,23 @@ const mySelect = document.getElementById("mySelect").value
 // column1.innerHTML = inputDate
 // column3.innerHTML = inputAmount
 // column2.innerHTML = mySelect
-const tableDiv = document.getElementById("tableDiv")
+let tableBody = document.getElementById("tableBody")
+let tr = document.createElement("TR")
+let th = document.createElement("TH")
+th.setAttribute("scope","row")
+let td1 = document.createElement("TD")
+let td2 = document.createElement("TD")
+let td3 = document.createElement("TD")
 
+tableBody.appendChild(tr)
+tr.appendChild(th)
+th.appendChild(td1)
+th.appendChild(td2)
+th.appendChild(td3)
 
-const table = document.getElementById("table")
-  tableDiv.appendChild(table)
-table.setAttribute("class","table")
-const thead = document.createElement("THEAD")
- table.appendChild(thead)
-
-
-const tr =  document.createElement("TR")
-tr.setAttribute("id","t-r")
-  thead.appendChild(tr)
-const th1 =  document.createElement("TH")
-
-
-
-
-
-
-th1.innerHTML = "#"
-th1.setAttribute("scope","col")
-const th2 = document.createElement("TH")
-th2.innerHTML = "Date"
-th2.setAttribute("scope","col")
-const th3 = document.createElement("TH")
-th3.innerHTML = "Amount"
-th3.setAttribute("scope","col")
-const th4 = document.createElement("TH")
-th4.innerHTML = "Category"
-th4.setAttribute("scope","col")
-
-
-tr.appendChild(th1)
-tr.appendChild(th2)
-tr.appendChild(th3)
-tr.appendChild(th4)
-
-
-let tableBody = document.createElement("TBODY")
-
-let tableRow1 = document.createElement("TR")
-  tableBody.appendChild(tableRow1)
-let th11 = document.createElement("TH")
-  tableRow1.appendChild(th11)
-  th11.setAttribute("scope","row")
-
-
-const td1 = document.createElement("TD")
-th11.appendChild(td1)
 td1.innerHTML = inputDate
-const td2 = document.createElement("TD")
-td2.innerHTML = inputAmount
-th11.appendChild(td2)
-const td3 = document.createElement("TD")
-td3.innerHTML = mySelect
-th11.appendChild(td3)
-
-
+td2.innerHTML = mySelect
+td3.innerHTML = inputAmount
 
 
 
